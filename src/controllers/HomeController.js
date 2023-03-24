c5a7084d34ce5ae66dd304bd5ebe673380a9d439
Viorel Robu <wwjd.vio@gmail.com>
2023-03-24 14:40:26 +0200

    home page framework to show and play recordings

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,3 +1,3 @@
 exports.index  = async (req, res) => {
-  res.render('index', { title: 'Stream Recorder' })
+  res.render('index', { title: 'Stream Recorder', streams: [{id: 1, name: 'rvesv', date: '02-02-2023', description: 'Inregistrare RVESV test'}] })
 }
